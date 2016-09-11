87253668ea2061f92953f6653fb5a53cf934c2fb
snowymo <snowymohehe@gmail.com>
2016-09-11 01:43:59 -0400

    fix the position and orientation of reference masks and give up the parable

---
+++ Holojam/Assets/Holojam/PhysicalSharing/Scripts/behavior/actorOffsetCtrl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class actorOffsetCtrl : MonoBehaviour {
+
+	Transform parent;
+
+	public GameObject currentTable;
+	public GameObject referenceTable;
+
+	// Use this for initialization
+	void Start () {
+		parent = transform.parent;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.position = Quaternion.Inverse(currentTable.transform.rotation) * referenceTable.transform.rotation * 
+			(parent.position - currentTable.transform.position) + referenceTable.transform.position;
+//		transform.rotation = parent.rotation * referenceTable.transform.rotation * Quaternion.Inverse(currentTable.transform.rotation);
+		transform.rotation =  Quaternion.Inverse(currentTable.transform.rotation) * referenceTable.transform.rotation * parent.rotation;
+//		transform.localPosition = - currentTable.transform.position + referenceTable.transform.position;
+//		transform.localRotation = referenceTable.transform.rotation * Quaternion.Inverse(currentTable.transform.rotation);
+
+	}
+}
---
+++ Holojam/Assets/Holojam/PhysicalSharing/Scripts/behavior/handOffsetCtrl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class handOffsetCtrl : MonoBehaviour {
+
+	Transform parent;
+	Transform grandParent;
+
+	public GameObject currentTable;
+	public GameObject referenceTable;
+
+	// Use this for initialization
+	void Start () {
+		parent = transform.parent;
+		grandParent = parent.parent;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		parent.position = grandParent.position - currentTable.transform.position + referenceTable.transform.position;
+		parent.rotation = grandParent.rotation * referenceTable.transform.rotation * Quaternion.Inverse(currentTable.transform.rotation);
+	}
+}
