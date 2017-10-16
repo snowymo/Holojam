57aa837feacf4125c6c6de4ea30186e6a1b3459e
Zhenyi He <snowymohehe@gmail.com>
2017-10-16 00:26:50 -0400

    python read openvr, node receive and send to relay

---
+++ Relay/udpServer_recv.js
@@ -0,0 +1,92 @@
+var udp = require('dgram');
+const holojam = require('holojam-node')(['relay'],'192.168.1.80');
+// --------------------creating a udp server --------------------
+
+// creating a udp server
+var server = udp.createSocket('udp4');
+
+// emits when any error occurs
+server.on('error',function(error){
+  console.log('Error: ' + error);
+  server.close();
+});
+
+valid_trackers = {
+	"LHR-1ADE44F5" : "RBTB",
+	"LHR-08DE8B86" : "P1RH",
+	"LHR-18DD003E" : "TBL",
+	"LHR-D5E02F77" : "P1HMD",
+	
+	"LHR-0DC35B0C" : "RBTA",
+	"LHR-08DFFC5D" : "TBL2",
+	"LHR-0ADD38C6" : "P2RH",
+	"LHR-08C154A6" : "P2HMD"
+};
+
+// emits on new datagram msg
+server.on('message',function(msg,info){
+  //console.log('Data received from client : ' + msg.toString());
+  //console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
+  //console.log('Data received from client : ' + msg);
+  var jsonObj = JSON.parse(msg.toString());
+  var liveObjs = []
+  
+  //console.log(valid_trackers);
+  
+  for (var key in jsonObj) {
+	if (!jsonObj.hasOwnProperty(key)) {
+	  continue;
+	}
+	if (key == 'time') {
+		continue;
+	}
+	
+	//console.log("jsonObj[key].id", jsonObj[key].id);
+	var validlabel = valid_trackers[jsonObj[key].id];
+	//console.log("validlabel",validlabel);
+	if(validlabel == "" || validlabel == undefined)
+		continue;
+	
+	var liveObj = {
+                  label: validlabel, 
+				  vector3s: [{x: parseFloat(jsonObj[key].x), y: parseFloat(jsonObj[key].y), z: -parseFloat(jsonObj[key].z)}],
+				  vector4s: [{x: parseFloat(jsonObj[key].qx), y: parseFloat(jsonObj[key].qy), z: -parseFloat(jsonObj[key].qz), w: -parseFloat(jsonObj[key].qw)}]
+               };
+    //console.log('live Obj to send' + JSON.stringify(liveObj));
+    liveObjs.push(liveObj);
+  }
+  //console.log(JSON.stringify(liveObjs));
+  holojam.Send(holojam.BuildUpdate('Vive',liveObjs));
+//sending msg
+// server.send(msg,info.port,'localhost',function(error){
+//   if(error){
+//     client.close();
+//   }else{
+//     console.log('Data sent !!!');
+//   }
+
+// });
+
+});
+
+//emits when socket is ready and listening for datagram msgs
+server.on('listening',function(){
+  var address = server.address();
+  var port = address.port;
+  var family = address.family;
+  var ipaddr = address.address;
+  console.log('Server is listening at port ' + port);
+  console.log('Server ip :' + ipaddr);
+  console.log('Server is IP4/IP6 : ' + family);
+});
+
+//emits after the socket is closed using socket.close();
+server.on('close',function(){
+  console.log('Socket is closed !');
+});
+
+server.bind(10000);
+
+// setTimeout(function(){
+// server.close();
+// },8000);
---
+++ Relay/updServer_send_singlemachine.py
@@ -0,0 +1,74 @@
+import triad_openvr
+import time
+import sys
+import socket
+import json
+
+v = triad_openvr.triad_openvr()
+v.print_discovered_objects()
+
+if len(sys.argv) == 1:
+    interval = 1/float(120)
+elif len(sys.argv) == 2:
+    interval = 1/float(sys.argv[0])
+else:
+    print("Invalid number of arguments")
+    interval = False
+
+valid_devices = ["tracker_1",
+                 "tracker_2",
+                 "tracker_3",
+                 "tracker_4",
+                 "tracker_5",
+                 "tracker_6",
+                 "tracker_7",
+                 "tracker_8",
+                 "tracker_9",
+                 "tracker_10",
+                 "tracker_11",
+                 "tracker_12",
+                 "tracking_reference_1",
+                 "tracking_reference_2",
+                 "controller_1",
+                 "hmd_1"]
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 10000
+MESSAGE = b"Hello, World!"
+ 
+print ("UDP target IP:", UDP_IP)
+print ("UDP target port:", UDP_PORT)
+ 
+sock = socket.socket(socket.AF_INET, # Internet
+                      socket.SOCK_DGRAM) # UDP
+data = {}
+if interval:
+    while(True):
+        start = time.time()
+        txt = ""
+        for device_key in v.devices.keys():
+            if device_key not in valid_devices:
+                continue
+            new_data = {}
+            data_ = v.devices[device_key].get_pose_quaternion()
+            new_data['name'] = device_key
+            new_data['id'] = v.devices[device_key].get_serial()
+            new_data['x'] = data_[0]
+            new_data['y'] = data_[1]
+            new_data['z'] = data_[2]
+            new_data['qw'] = data_[3]
+            new_data['qx'] = data_[4]
+            new_data['qy'] = data_[5]
+            new_data['qz'] = data_[6]
+            print("sending:", new_data)
+            data[device_key] = new_data
+
+        data['time'] = time.time()
+        jsondata = json.dumps(data)
+        
+        
+        sock.sendto(jsondata.encode('utf-8'), (UDP_IP, UDP_PORT))
+        
+        sleep_time = interval-(time.time()-start)
+        if sleep_time>0:
+            time.sleep(sleep_time)
